08c92637b6cb912b088e55ab6d2a55a1c014ef37
CoreNodes <corenodes1@gmail.com>
2022-03-26 16:45:15 +0100

    remove border

--- components/Buttons/ButtonTable.tsx
+++ components/Buttons/ButtonTable.tsx
@@ -8,7 +8,7 @@ const ButtonTable: React.FC<{
   return (
     <button
       disabled={loading}
-      className="mr-4 px-2 py-1  border rounded-md  bg-slate-400 text-white hover:bg-slate-500 hover:text-white disabled:bg-slate-600 disabled:text-white dark:bg-gradient-to-tr dark:from-slate-600 dark:to-slate-700 hover:dark:from-slate-700 dark:disabled:from-slate-800 dark:disabled:to-slate-900"
+      className="mr-4 px-2 py-1 rounded-md  bg-slate-400 text-white hover:bg-slate-500 hover:text-white disabled:bg-slate-600 disabled:text-white dark:bg-gradient-to-tr dark:from-slate-600 dark:to-slate-700 hover:dark:from-slate-700 dark:disabled:from-slate-800 dark:disabled:to-slate-900"
       onClick={async (e) => {
         setLoading(true);
         await props.onHandleClick(e);
